e0eabd38fb151e732a44131290ae3584daae5221
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-12-16 09:35:17 -0300

    Projeto screnMacth lista de filmes iniciado

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+>
+src/Filme.java,7\a\7a349d7d8c53f29127d0b3d798fc4a91f3f6416f
---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+//A Classe irá especificar o que o filme tem
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
