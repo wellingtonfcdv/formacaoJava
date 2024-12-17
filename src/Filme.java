f2e2a17dfbd06bd372fa805f0cea9fbd0fdda1b3
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-12-17 09:31:31 -0300

    Criação dos métodos na classe filme

--- src/Filme.java
+++ src/Filme.java
@@ -3,7 +3,23 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaAvaliacao;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    // Se o método não retorna nada, deve definir como void
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaAvaliacao += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaAvaliacao / totalDeAvaliacoes;
+    }
+
 }
