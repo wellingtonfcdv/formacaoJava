ebde542d409a104479f482571ca231a90492b126
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-12-17 10:59:26 -0300

    Organizando código e criação de pacote

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,29 +1,31 @@
+package br.com.alura.screenmatch.modelos;
+
 //A Classe irá especificar o que o filme tem
 public class Filme {
-    String nome;
+    public String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
     private double somaAvaliacao;
     private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
-    int getTotalDeAvaliacoes(){
+    public int getTotalDeAvaliacoes(){
         return  totalDeAvaliacoes;
     }
 
     // Se o método não retorna nada, deve definir como void
 
-    void exibeFichaTecnica(){
+    public void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
-    void avalia(double nota){
+    public void avalia(double nota){
         somaAvaliacao += nota;
         totalDeAvaliacoes++;
     }
 
-    double pegaMedia(){
+    public double pegaMedia(){
         return somaAvaliacao / totalDeAvaliacoes;
     }
 
