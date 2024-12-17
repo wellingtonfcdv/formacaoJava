f43647ff8741c09baff3156f5fd07237e027fe15
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-12-17 10:32:10 -0300

    Configurando o que pode ser visto e modificado.

--- src/Filme.java
+++ src/Filme.java
@@ -3,11 +3,16 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double somaAvaliacao;
-    int totalDeAvaliacoes;
+    private double somaAvaliacao;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    int getTotalDeAvaliacoes(){
+        return  totalDeAvaliacoes;
+    }
+
     // Se o método não retorna nada, deve definir como void
+
     void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
--- src/Principal.java
+++ src/Principal.java
@@ -9,8 +9,8 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaAvaliacao);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+
+        System.out.println("Total de avaliações: "+meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
