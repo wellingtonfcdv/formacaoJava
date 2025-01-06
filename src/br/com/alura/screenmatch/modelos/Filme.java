91f6c648810957143abd011585d3274e4d6368ec
Wellington Feijo <p-wcosta@tce.sp.gov.br>
2025-01-06 14:34:28 -0300

    Criação do construtor que recebe o nome do filme na criação

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -7,6 +7,11 @@ public class Filme  extends Titulo implements Classificavel {
 
     private String diretor;
 
+    public Filme(String nome) {
+        this.setNome(nome);
+    }
+
+
     public String getDiretor() {
         return diretor;
     }
